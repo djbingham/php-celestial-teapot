2cf77133593937716bf97ffccff594e5ba1e14f6
David Bingham <git@davidjbingham.co.uk>
2015-08-26 18:55:28 +0100

    Capitalised first letter of folder names throughout project, to match namespaces.

--- Test/bootstrap.php
+++ Test/bootstrap.php
@@ -3,6 +3,6 @@
 namespace SlothMySql\Test;
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/utility/Autoload.php';
+require_once __DIR__ . '/Utility/Autoload.php';
 
 new Utility\Autoload(dirname(__DIR__), 'SlothMySql');
