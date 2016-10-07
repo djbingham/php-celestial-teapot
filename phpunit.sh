4f7e579996a51fcf34b1f0157f34a48d0d895e60
David Bingham <git@davidjbingham.co.uk>
2016-10-07 09:20:53 +0100

    Updated phpunit.sh to use docker-compose in the Test directory, rather pulling the vanilla PHPUnit image from Docker Hub.

--- phpunit.sh
+++ phpunit.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
-docker pull phpunit/phpunit
-docker run -v $(pwd):/app --rm phpunit/phpunit --configuration=Test/phpunit.xml --bootstrap=Test/bootstrap.php $@
+
+pushd "$( dirname "${BASH_SOURCE[0]}" )/Test" > /dev/null
+
+docker-compose up unit-test
+docker-compose up -d database
+sleep 11 # Wait for database to be ready. Yes, there are almost certainly more reliable methods for this.
+docker-compose up integration-test
+docker-compose stop
+docker-compose rm -vf
+
+popd
