1e8dc0646ab89e3e848ba1310495fe7fdd86ff2e
David Bingham <git@davidjbingham.co.uk>
2016-10-07 17:31:09 +0100

    Updated demo script to use PDO.

--- demo.php
+++ demo.php
@@ -1,16 +1,9 @@
 <?php
 // Setup a database wrapper
-$database = new SlothMySql\Connection\Database(array(
-	'host' => 'localhost',
-	'username' => 'slothMySqlDemoUser',
-	'password' => 'slothMySqlD3m0P4ss',
-	'name' => 'slothMySqlDemo',
-	'port' => null,
-	'socket' => null
-));
-$dbConnection = new SlothMySql\Connection\MySqli($database);
-$queryBuilder = new SlothMySql\QueryBuilder\Wrapper($dbConnection);
-$db = new SlothMySql\DatabaseWrapper($dbConnection, $queryBuilder);
+$pdo = new \PDO('mysql:host=database;dbname=test_db', 'test_user', 'test_pass');
+$pdoWrapper = new PhpMySql\Connection\PdoWrapper($pdo);
+$queryBuilder = new PhpMySql\QueryBuilder\Wrapper($pdoWrapper);
+$db = new PhpMySql\DatabaseWrapper($pdoWrapper, $queryBuilder);
 
 // Example query construction
 $table1 = $db->value()->table('MyFirstTable');
@@ -19,7 +12,7 @@ $sampleQuery = $db->query()
 	->select()
 	->field($table1->field('firstField')->setAlias('firstTableField'))
 	->from($table1)
-	->join($db->query()->join()->inner()
+	->join($db->join()->inner()
 		->table($table2)
 		->withAlias('MySecondTableAlias')
 		->on($db->query()->constraint()
