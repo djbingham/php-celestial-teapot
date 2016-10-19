e19f980ee5ab3953b1f07bc2c4c947ccba8bcd13
David Bingham <git@davidjbingham.co.uk>
2016-10-19 09:26:36 +0100

    Removed dependence on custom favicon/autoload project, in favour of using Composer's custom autoload functionality.

--- Test/bootstrap.php
+++ Test/bootstrap.php
@@ -1,9 +1,3 @@
 <?php
 
-namespace Test;
-
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/Utility/Autoload.php';
-
-new Utility\Autoload(dirname(__DIR__) . '/PhpMySql', 'PhpMySql');
-new Utility\Autoload(dirname(__DIR__) . '/Test', 'Test');
